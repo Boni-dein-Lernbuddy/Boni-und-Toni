d3d96fdeac84d980d8d3387ddef5a5f13a9c49ec
Jana Esselborn <janaesselborn@gmail.com>
2020-06-09 21:15:59 +0200

    Create home view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { StudentComponent } from './student/student/student.component';
 import { TeacherComponent } from './teacher/teacher/teacher.component';
+import {HomeViewComponent} from "./home-view/home-view/home-view.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'teacher',
     component: TeacherComponent
+  },
+  {
+    path: '',
+    component: HomeViewComponent
   }
 ];
 @NgModule({
