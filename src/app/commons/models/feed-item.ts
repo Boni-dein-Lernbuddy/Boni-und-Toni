cc455e2e01347505fee0f70701ee1e3f68c0daa0
kvn-shn <66770766+kvn-shn@users.noreply.github.com>
2020-06-11 12:25:02 +0200

    Feature/student view tweaks (#7)
    * support HTML in card content
    
    * add new feed items
    
    * make mocked date more realistic
    
    * add "mark as done" button to tasks
    
    * extend "general" mock
    
    * improve "mark as done" button
    
    * add "has updated" badge
    
    Co-authored-by: Kevin Schneider <kevin.schneider@iteratec.de>

--- src/app/commons/models/feed-item.ts
+++ src/app/commons/models/feed-item.ts
@@ -1,7 +1,13 @@
 export interface FeedItem {
     title?: string;
+
+    /** URI to picture, external website etc. depending on `type`. */
     attachmentPath?: string;
-    createDate: Date;
+
+    createDate?: Date;
+
+    /** Textual content. *Save* HTML elements are allowed. */
     content: string;
-    type: 'text' | 'task' | 'picture' | 'video' | 'audio' | 'link' | 'document';
+
+    type: 'text' | 'task' | 'picture' | 'video' | 'audio' | 'link' | 'document' | 'info';
 }
--- src/app/commons/service/folders-mock.service.ts
+++ src/app/commons/service/folders-mock.service.ts
@@ -10,33 +10,41 @@ export class FoldersMockService {
   constructor() { }
 
   getFolderStructure(): Folder {
-    let root: Folder = { name: 'Boni', children: [] };
+    const root: Folder = { name: 'Boni', children: [] };
 
-    let geography: Folder = { name: 'Erdkunde', parent: root, children: [] };
-    let maths: Folder = { name: 'Mathe', parent: root, children: [] };
+    const geography: Folder = { name: 'Erdkunde', parent: root, children: [] };
+    const maths: Folder = { name: 'Mathe', parent: root, children: [] };
 
     const rootChildren: Folder[] = [geography, maths];
 
-    let geoGeneral: Folder = { name: 'Allgemeines', parent: geography, children: [] };
-    let geoIceberg: Folder = { name: 'Gletscher', parent: geography, children: [] };
+    const geoGeneral: Folder = { name: 'Allgemeines', parent: geography, isUpdated: true, children: [] };
+    const geoIceberg: Folder = { name: 'Gletscher', parent: geography, isUpdated: true, children: [] };
+    const geoTopography: Folder = { name: 'Topographie', parent: geography, children: [] };
 
-    const geoChildren: Folder[] = [geoGeneral, geoIceberg];
+    const geoChildren: Folder[] = [geoGeneral, geoIceberg, geoTopography];
 
     root.children = rootChildren;
     geography.children = geoChildren;
 
-    const now = new Date();
+    geoGeneral.childFeed = [{type: 'info', content: 'Die Videokonferenz findet heute erst 15 Minuten später statt. Start ist um <b>15:15 Uhr</b>.'}];
 
-    const iceFeed1: FeedItem = { createDate: now, content: "Guten Morgen! Heute wollen wir uns mit dem Thema \"Entstehung und Aufbau von Gletschern\" beschäftigen.", type: "text" }
-    const iceFeed2: FeedItem = { title: "Was sind Gletscher?", attachmentPath: "../../../assets/images/ice-min.jpg", createDate: now, content: "lorem ipsum", type: 'picture' };
-    const iceFeed3: FeedItem = { createDate: now, content: "Hier könnte eine Aufgabe stehen.", type: 'text' };
-    const iceFeed4: FeedItem = { createDate: now, content: "Ein kurzes Video zu Gletschern", type: 'video', attachmentPath: '../../../assets/videos/Glacier.mp4'};
-    const iceFeed5: FeedItem = { createDate: now, content: "Wikipedia Artikel zu Gletschern", type: 'link', attachmentPath: 'https://de.wikipedia.org/wiki/Gletscher'}
+    const iceFeed1: FeedItem = { createDate: now(-44), content: 'Guten Morgen! Heute wollen wir uns mit dem Thema "Entstehung und Aufbau von Gletschern" beschäftigen.', type: 'text' };
+    const iceFeed2: FeedItem = { title: 'Was sind Gletscher?', attachmentPath: '../../../assets/images/ice-min.jpg', createDate: now(-43), content: 'lorem ipsum', type: 'picture' };
+    const iceFeed3: FeedItem = { createDate: now(-41), content: 'Hier könnte eine Aufgabe stehen.', type: 'text' };
+    const iceFeed4: FeedItem = { createDate: now(-37), content: 'Ein kurzes Video zu Gletschern', type: 'video', attachmentPath: '../../../assets/videos/Glacier.mp4' };
+    const iceFeed5: FeedItem = { createDate: now(-15), content: 'Wikipedia-Artikel zu Gletschern', type: 'link', attachmentPath: 'https://de.wikipedia.org/wiki/Gletscher' };
+    const iceFeed6: FeedItem = { createDate: now(-10), content: 'Unter diesem Link findet ihr Aufgaben zu diesem Thema: <a href="https://link-zur-aufgabe.de">link-zur-aufgabe.de</a>', type: 'task' };
+    const iceFeed7: FeedItem = { createDate: now(), content: 'Eine Videokonferenz zum Thema findet morgen um <b>15:00 Uhr</b> statt.', type: 'info' };
 
-    const iceFeedItems = [iceFeed1, iceFeed2, iceFeed3, iceFeed4, iceFeed5];
+    const iceFeedItems = [iceFeed1, iceFeed2, iceFeed3, iceFeed4, iceFeed5, iceFeed6, iceFeed7];
 
     geoIceberg.childFeed = iceFeedItems;
 
     return root;
   }
+}
+
+function now(modifyMinutes = 0): Date {
+  const date = new Date();
+  return new Date(date.setMinutes(date.getMinutes() + modifyMinutes));
 }
