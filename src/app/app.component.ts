40306861cae112135c8460ba68c909452eeaf5af
Jana Esselborn <44244386+janaesselborn@users.noreply.github.com>
2020-06-12 12:56:44 +0200

    Feature/optimize home screen and s martphone mockup (#16)
    * Get mobile view on desktop
    
    * Optimize Home View

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Location} from '@angular/common';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private location: Location,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -23,5 +27,13 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  goHome() {
+    this.router.navigate(["/"]);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
-import { CommonModule, registerLocaleData } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import {CommonModule, registerLocaleData} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
 import localeDe from '@angular/common/locales/de';
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CommonsModule} from "./commons/commons.module";
+import {HomeViewComponent} from "./home-view/home-view/home-view.component";
 
 registerLocaleData(localeDe);
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, HomeViewComponent],
     imports: [
         AppRoutingModule,
         BrowserModule,
         CommonModule,
+        CommonsModule,
         HttpClientModule,
         IonicModule.forRoot()
     ],
@@ -35,4 +38,5 @@ registerLocaleData(localeDe);
     ],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
